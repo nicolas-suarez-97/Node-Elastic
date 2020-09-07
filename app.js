6ca67e6f52e56f997b4f94fd9bc94483d2b27441
nicolas-suarez-97 <nicolas.suarez10@hotmail.com>
2020-09-07 15:34:51 -0500

    Lambda Deploy

---
+++ app.js
@@ -0,0 +1,24 @@
+// const serverless = require('serverless-http');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json({ extended: true }));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+const PORT = process.env.PORT || 8000;
+
+app.use('/api/employe', require('./routes/employe'));
+
+app.listen(PORT, () => {
+    console.log(`El servidor está funcionando en el puerto ${PORT}`);
+});
+
+
+module.exports = app;
+// module.exports.handler = serverless(app);
+
